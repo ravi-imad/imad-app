96af736680b0cedab1ca8b6f17feac856238200c
ravi-imad <30843805+ravi-imad@users.noreply.github.com>
2017-08-20 17:21:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,11 @@
-console.log('Loaded!');
+//button
 
+var button = document.getElementById('counter');
 
-//move element
-var img = document.getElementById('madi');
-var marginLeft=0;
+var counter=0;
 
-function moveRight() {
-    marginLeft += 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
+button.onclick = function(){
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
