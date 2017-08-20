608e4729c99b9d57c1d571109624b07dc3c14585
ravi-imad <30843805+ravi-imad@users.noreply.github.com>
2017-08-20 16:36:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
 var element = document.getElementById('main-text');
+
 element.innerHTML = "New Value";
+
+//move element
+var img = document.getElementById('madi');
+
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
